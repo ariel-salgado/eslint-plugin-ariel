e82e63591e54a3b341d608b0fa3d4cb095dc0610
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-10 20:56:48 -0400

    chore: added indentation

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-  entry: ['./src/index.ts'],
-  dts: {
-    tsconfig: './tsconfig.build.json',
-    tsgo: true,
-  },
+	entry: ['./src/index.ts'],
+	dts: {
+		tsconfig: './tsconfig.build.json',
+		tsgo: true,
+	},
 })
